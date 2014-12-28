2964c4361fe41e4e507f8e14daf89ef9c914a7ee
danielegenta <danielegenta@icloud.com>
2014-12-28 22:19:16 +0100

    0.3
    Al caricamento del DOM:
    -Creazione di 16 sotto-div figli del div padre contenitore
    -Abozzo css div (padre e figli)

--- Script/script.js
+++ Script/script.js
@@ -1,4 +1,17 @@
-function prova()
-{
-	alert("prova");
-}
+//Al caricamento del DOM inizializzo la tabella (creo sotto-div)
+$(document).ready(function(){
+	for (var i = 1; i < 17; i++) {
+    	var cl = "nm";
+        var cn = i;
+        if(i == 16) 
+        {
+            cn = "X";
+        }
+        var div = $('<div />');
+        div.attr("id",i);
+        //div.attr("onclick","move(" + i + ")");
+        div.addClass(cl);
+        div.html(cn);
+        $("#mainTable").append(div);
+        }
+});
