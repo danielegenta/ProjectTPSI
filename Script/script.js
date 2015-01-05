b3eb164826e885e5cfdbb8d4ae6062c6fc540a8f
danielegenta <danielegenta@icloud.com>
2015-01-05 23:25:01 +0100

    6.0
    -Ottimizzazione timer (da ms a secondi)
    -Miglioramento to-do list
    -Semplificazione script (rinomina variabili)
    -Documentazione codice (commenti)

--- Script/script.js
+++ Script/script.js
@@ -1,13 +1,16 @@
 //Al caricamento del DOM inizializzo la tabella (creo sotto-div)
 //Variabili per cronometro
-var start = "00:00.00";
-    var go = false;
-    var speed = 10;
-    var a,b,c,d,e,f,u;
+var stringaTempo = "00:00";
+var scorriTempo = false;
+var velocitaCronometro = 1000;
+var decineMinuti,unitaMinuti,decimiSecondo,centesimiSecondo,e,f,separatoreMinSec;
+//Setto impostazioni
 $(document).ready(function()
 {	
 	var i=0;
-    avvia();
+	//Avvio cronometro
+    switchCronometro();
+    //Riempo il contenitore principale con le caselle (div)
 	for (i = 0; i < 16; i++) 
 	{	
     	var classeDiv = "celleTabella";
@@ -25,12 +28,17 @@ $(document).ready(function()
         cella.attr("onclick","clickCell(" +i+ ")");
         cella.addClass(classeDiv);
         cella.html(contenutoDiv);
+        //Appendo al padre i div figli
         $("#mainTable").append(cella);
         //fare mescolamento
     }
+    //Mescolo le celle per iniziare la partita da uno stato di non-vincita
+    mescolaCelle();
+    //Evento click su mescola celle
     $("#btnMescola").click(function(){
     	mescolaCelle();
     });
+    //Evento click su nuova partita
     $("#btnNuovaPartita").click(function(){
     	mescolaCelle();
     	nuovaPartita();
@@ -38,12 +46,15 @@ $(document).ready(function()
 });
 
 //Ottimizzare
+//Evento click su una cella della tabella
   function clickCell(id) 
   {
   	var aumentaMosse=true;
+  	//Recupero il numero di mosse corrente
   	var numeroMosse=parseInt($("#mosse").text())
-  		//alert($("#mainTable").children().eq(id+1).text());
+  	//Variabile ausiliaria utile allo scambio del testo
 	var testo=$("#mainTable").children().eq(id).text();
+	//Spostamenti sinistra, destra, giu o su
 	if ($("#mainTable").children().eq(id+1).text()=="X" && id<16)
 	{
 		$("#mainTable").children().eq(id+1).html(testo);
@@ -64,22 +75,21 @@ $(document).ready(function()
 		$("#mainTable").children().eq(id-4).html(testo);
 		$("#mainTable").children().eq(id).html("X");
 	}
+	//Se nessuna cella è stata spostata non incremento il contatore di mosse, viceversa lo aumento di uno e controllo se ho vinto
 	else
 		aumentaMosse=false;
-	//aumento contatore mosse
 	if (aumentaMosse==true)
-	{
-		
+	{		
 		numeroMosse+=1;
 		$("#mosse").html(numeroMosse);
-		//controllo vincita
 		controlloVincita();
 	}
+	//Ripristino lo stato della variabile ausiliatia
 	aumentaMosse=true;
-	
    }
   
 //Ottimizzare con un while 
+//Controllo la vincita mediante il confronto fra il testo e gli id (che sono ordinati in modo crescente)
 function controlloVincita()
 {
 	var vincita=true;
@@ -88,80 +98,86 @@ function controlloVincita()
 		if ($("#mainTable").children().eq(i).text()!=parseInt($("#mainTable").children().eq(i).attr("id"))+1)
 			vincita=false;
 	}
+	//Se ho vinto fermo il cronometro e mostro i dettagli della partita
 	if (vincita==true)
 	{
 		alert("hai vinto (aggiungere dettagli: numero mosse e tempo");
 		alert("Clicca su nuova partita per iniziare un altro match!")
-		avvia();
+		switchCronometro();
 	}
 }   
 
+//Funzione che si occupa di mescolare le celle (testo)
 function mescolaCelle() {}
+
+//Funzione che inizializza le impostazione della partita
 function nuovaPartita()
 {
-//reset timer e mosse
 	$("#mosse").html(0);
-	avvia();
+	switchCronometro();
 }
 
 
-//cronometro
+//Funzioni legate a cronometro
 //tutto da rivedere ottimizzare
-function avvia()
+//Interruttore aziona/ferma cronometro
+function switchCronometro()
 {   	
-        if(go == false) {            
-            start = "00:00.00";
+        if(scorriTempo == false) 
+        {            
+            stringaTempo = "00:00";
             cnt = setInterval(function() {
-                gostart();
-            },speed);
-        }else{
-            go = false;
+                avviaCronometro();
+            },velocitaCronometro);
+        }
+        else
+        {
+            scorriTempo = false;
             clearInterval(cnt);
         }        
 }
-function gostart(){
-	go = true;
-    a = parseInt(start.charAt(0));
-    b = parseInt(start.charAt(1));
-    u = ":";
-    c = parseInt(start.charAt(3));
-    d = parseInt(start.charAt(4));
-    e = ".";
-    f = parseInt(start.charAt(6));
-    g = parseInt(start.charAt(7));
-        if(g >= 9) {
-            g = 0;
-            if(f >= 9) {
-                f = 0;
-                if(d >= 9) {
-                    d = 0;
-                    if(c >= 5) {
-                        c = 0;
-                        if(b >= 9) {
-                            b = 0;
-                            if(a >= 9) {
+
+//Misura il tempo e lo stampa
+function avviaCronometro(){
+	scorriTempo = true;
+    decineMinuti = parseInt(stringaTempo.charAt(0));
+    unitaMinuti = parseInt(stringaTempo.charAt(1));
+    separatoreMinSec = ":";
+    decimiSecondo = parseInt(stringaTempo.charAt(3));
+    centesimiSecondo = parseInt(stringaTempo.charAt(4));
+                if(centesimiSecondo >= 9) {
+                    centesimiSecondo = 0;
+                    if(decimiSecondo >= 5) {
+                        decimiSecondo = 0;
+                        if(unitaMinuti >= 9) {
+                            unitaMinuti = 0;
+                            if(decineMinuti >= 9)
+                            {
                                 clearInterval(cnt);
-                            }else{
-                                a++;
                             }
-                        }else{
-                            b++;
+                            else
+                            {
+                                decineMinuti++;
+                            }
                         }
-                    }else{
-                        c++;
+                        else
+                        {
+                            unitaMinuti++;
+                        }
                     }
-                }else{
-                    d++;
+                    else
+                    {
+                        decimiSecondo++;
+                    }
                 }
-            }else{
-                f++;
-            }
-        } else {
-            g++;
-        }
-        start = String(a) + String(b) + String(u) + String(c) + String(d) + String(e) + String(f) + String(g);
-        for ( var i = 0; i < start.length; i++ ) {
-            $("#s" + i).html(start.charAt(i))
+                else
+                {
+                    centesimiSecondo++;
+                }
+        //Stampo il tempo
+        stringaTempo = String(decineMinuti) + String(unitaMinuti) + String(separatoreMinSec) + String(decimiSecondo) + String(centesimiSecondo);
+        for ( var i = 0; i < stringaTempo.length; i++ ) {
+            $("#s" + i).html(stringaTempo.charAt(i))
         }
 }
  
